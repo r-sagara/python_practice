a8b08941c7dc39d3298afbe19c57b76abc3785b3
SharkGrade <r.kushnir@do-it.co>
2018-09-30 00:01:10 +0300

    First example is created

---
+++ hw1.py
@@ -0,0 +1,24 @@
+i = 1
+while i <= 10:
+    if i == 1:
+        print("Верно" if input("Question 1: ") == "10" else "Неверно")
+    elif i == 2:
+        print("Верно" if input("Question 2: ") == "20" else "Неверно")
+    elif i == 3:
+        print("Верно" if input("Question 3: ") == "30" else "Неверно")
+    elif i == 4:
+        print("Верно" if input("Question 4: ") == "40" else "Неверно")
+    elif i == 5:
+        print("Верно" if input("Question 5: ") == "50" else "Неверно")
+    elif i == 6:
+        print("Верно" if input("Question 6: ") == "60" else "Неверно")
+    elif i == 7:
+        print("Верно" if input("Question 7: ") == "70" else "Неверно")
+    elif i == 8:
+        print("Верно" if input("Question 8: ") == "80" else "Неверно")
+    elif i == 9:
+        print("Верно" if input("Question 9: ") == "90" else "Неверно")
+    elif i == 10:
+        print("Верно" if input("Question 10: ") == "100" else "Неверно")
+    i = i + 1
+
